ea15bb587965f42766afccfb757c3ef7016b6f1e
4b2g0107@office.stust.edu.tw <4b2g0107@office.stust.edu.tw>
2024-12-31 15:41:14 +0800

    調整視窗佈局並新增功能
    修改了 MainWindow.xaml 的視窗尺寸，新增了 Grid 佈局和多個 UI 元素，包括 StackPanel、TextBox、GridSplitter、Grid 和 StatusBar。移除了 MainWindow.xaml.cs 中未使用的 using 指令，並新增了 defaultURL 字串和 GetAQIButton_Click 事件處理器。
    
    更新 MainWindow 佈局並新增功能
    
    修改了 MainWindow.xaml 的 Title、Height 和 Width 屬性，將高度設為 1000，寬度設為 1200。新增了 Grid 的 RowDefinitions 和 ColumnDefinitions，分別定義了三行和三列的佈局。新增了一個 StackPanel，位於第一行，橫跨三列，內含一個 Label 和一個包含 TextBox 和 Button 的 StackPanel。新增了一個 TextBox，位於第二行第一列，用於顯示內容。新增了一個 GridSplitter，位於第二行第二列，用於調整列寬。新增了一個 Grid，位於第二行第三列，背景色為 AliceBlue。新增了一個 StatusBar，位於第三行，橫跨三列，內含一個 StatusBarItem 和一個 TextBlock，顯示狀態訊息。
    
    在 MainWindow.xaml.cs 中，移除了多個未使用的 using 指令。新增了一個 defaultURL 字串變數，並在 MainWindow 的建構函式中將其設為 UrlTextBox 的預設值。新增了一個 GetAQIButton_Click 事件處理函式，當按下按鈕時，將 ContentTextBox 的文字設為 "抓取資料中..."。

--- WpfApp7/MainWindow.xaml.cs
+++ WpfApp7/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WpfApp7
 {
@@ -16,9 +7,16 @@ namespace WpfApp7
     /// </summary>
     public partial class MainWindow : Window
     {
+        string defaultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=ImportDate%20desc&format=JSON";
         public MainWindow()
         {
             InitializeComponent();
+            UrlTextBox.Text = defaultURL;
+        }
+
+        private void GetAQIButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTextBox.Text = "抓取資料中...";
         }
     }
 }
